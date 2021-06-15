88322a3d2555c9d4ba077e20b4f98bb220100e86
Brayan_Restrepo <brayan.2293@gmail.com>
2021-06-15 08:55:31 -0500

    django_heroku

--- omniLatam_ecommerce/settings.py
+++ omniLatam_ecommerce/settings.py
@@ -182,6 +182,3 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
 }
-
-import django_heroku
-django_heroku.settings(locals())
